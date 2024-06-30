6103e09333f0920ef581568e375ade131a154283
Piotr Prosol <peter.prosol@gmail.com>
2024-06-30 16:06:08 -0400

    Update model

--- create_mnist_model.py
+++ create_mnist_model.py
@@ -8,15 +8,18 @@ import tensorflowjs as tfjs
 train_images = train_images.reshape(-1, 28 * 28) / 255.0
 test_images = test_images.reshape(-1, 28 * 28) / 255.0
 
-def create_model():
+def create_cnn_model():
     model = tf.keras.Sequential([
-        keras.layers.Dense(512, activation='relu', input_shape=(784,)),
-        keras.layers.BatchNormalization(),
-        keras.layers.Dropout(0.3),
+        keras.layers.Reshape((28, 28, 1), input_shape=(784,)),
+        keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        keras.layers.MaxPooling2D((2, 2)),
+        keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        keras.layers.MaxPooling2D((2, 2)),
+        keras.layers.Flatten(),
         keras.layers.Dense(256, activation='relu'),
         keras.layers.BatchNormalization(),
         keras.layers.Dropout(0.3),
-        keras.layers.Dense(20, activation='softmax')
+        keras.layers.Dense(10, activation='softmax')
     ])
 
     model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001),
@@ -25,11 +28,12 @@ def create_model():
 
     return model
 
+
 # Create a model instance
-model = create_model()
+model = create_cnn_model()
 
 # Train the model
-model.fit(train_images, train_labels, epochs=10, batch_size=64)
+model.fit(train_images, train_labels, epochs=20, batch_size=32)
 
 # Evaluate the model
 loss, acc = model.evaluate(test_images, test_labels, verbose=2)
